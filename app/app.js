0ccd1e9dbf31380daa88419afe5d54375d60aa39
Cong Wang <cngwang6@gmail.com>
2016-11-03 00:45:18 -0400

    fb7

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Feed from './components/feed';
+import { IndexRoute, Router, Route, browserHistory } from 'react-router'
 // class Feed extends React.Component {
 // 	render() {
 // 		return(
@@ -8,7 +9,33 @@ import Feed from './components/feed';
 // 		);
 // 	}
 // }
-ReactDOM.render(
-	<Feed user={4} />,
-	document.getElementById('fb-feed')
-);
+class ProfilePage extends React.Component{
+	render(){
+		return(
+			<p>This is the profile page for a user with ID {this.props.params.id}</p>);
+	}
+}
+class FeedPage extends React.Component{
+	render(){
+		return <Feed user={4}/>;
+	}
+}
+class App extends React.Component{
+	render(){
+		return (
+			<div>{this.props.children}</div>)
+	}
+}
+ReactDOM.render((
+  <Router history={ browserHistory }>
+    <Route
+           path="/"
+           component={ App }>
+      { /* Show the Feed at / */ }
+      <IndexRoute component={ FeedPage } />
+      <Route
+             path="profile/:id"
+             component={ ProfilePage } />
+    </Route>
+  </Router>
+  ), document.getElementById('fb-feed'));
